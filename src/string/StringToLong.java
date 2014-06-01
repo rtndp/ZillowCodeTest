366a278b6bb427cd4aebfb42237cdf09a1570944
Ratnadeep Mangalvedhekar <rm7@umbc.edu>
2014-05-31 22:47:05 -0400

    initial

---
+++ src/string/StringToLong.java
@@ -0,0 +1,141 @@
+package string;
+
+public class StringToLong {
+
+	/**
+	 * Method to convert String to long
+	 * 
+	 * @param charSeq
+	 * @return
+	 * @throws NumberFormatException
+	 * @throws Exception
+	 */
+	public static long stringToLong(char[] charSeq)
+			throws NumberFormatException, Exception {
+
+		if (charSeq == null)
+			throw new NumberFormatException("EXCEPTION - input string is null");
+
+		if (charSeq.length == 0)
+			throw new NumberFormatException("EXCEPTION - input string is empty");
+
+		boolean negative = false;
+		int position = 0;
+		int length = charSeq.length;
+		int dec = 0;
+		long longValue = 0;
+
+		if (charSeq[0] == '-' || charSeq[0] == '+') {
+			if (charSeq[0] == '-')
+				negative = true;
+
+			position = 1;
+			length--;
+		}
+
+		while (length > 0) {
+			longValue *= 10;
+
+			dec = charSeq[position] - 48;
+
+			if (dec > 9 || dec < 0)
+				throw new NumberFormatException(
+						"EXCEPTION - Illegal characters in input string. "
+								+ "The String should contain characters between 0 - 9");
+
+			longValue += dec;
+
+			position++;
+			length--;
+		}
+
+		if (!negative && longValue < 0) {
+			throw new NumberFormatException(
+					"EXCEPTION - Out of range after conversion. MAX value is "
+							+ Long.MAX_VALUE);
+		}
+
+		if (negative && longValue == -longValue) {
+			return longValue;
+		}
+
+		if (negative && longValue < 0) {
+			throw new NumberFormatException(
+					"EXCEPTION - Out of range after conversion. MIN value is "
+							+ Long.MIN_VALUE);
+		}
+
+		return negative ? -longValue : longValue;
+	}
+
+	public static void main(String[] args) {
+		try {
+			
+			
+			System.out.println("MAX : " + Long.MAX_VALUE);
+			System.out.println("MIN : " + Long.MIN_VALUE);
+			System.out.println();
+
+			String inputString = "-9223372036854775808";
+			System.out.println("\nINPUT: " + inputString);
+			long ans = stringToLong(inputString.toCharArray());
+			System.out.println("String " + inputString + " in Long is " + ans);
+
+			/*
+			 * inputString = "-9223372036854775807";
+			 * System.out.println("INPUT: " + inputString); ans =
+			 * stringToLong(inputString.toCharArray());
+			 * System.out.println("String " + inputString + " in Long is " +
+			 * ans);
+			 */
+
+			/*
+			 * inputString = "-9223372036854775809";
+			 * System.out.println("INPUT: " + inputString); ans =
+			 * stringToLong(inputString.toCharArray());
+			 * System.out.println("String " + inputString + " in Long is " +
+			 * ans);
+			 */
+
+			inputString = "1L"; 
+			
+			System.out.println("\nINPUT: " + inputString);
+			ans = stringToLong(inputString.toCharArray());
+			System.out.println("String " + inputString + " in Long is " + ans);
+
+			/*
+			 * inputString = "9223372036854775807"; System.out.println("INPUT: "
+			 * + inputString); ans = stringToLong(inputString.toCharArray());
+			 * System.out.println("String " + inputString + " in Long is " +
+			 * ans);
+			 */
+
+			/*
+			 * inputString = "9223372036854775806"; System.out.println("INPUT: "
+			 * + inputString); ans = stringToLong(inputString.toCharArray());
+			 * System.out.println("String " + inputString + " in Long is " +
+			 * ans);
+			 */
+
+			/*
+			 * inputString = "9223372036854775808"; System.out.println("INPUT: "
+			 * + inputString); ans = stringToLong(inputString.toCharArray());
+			 * System.out.println("String " + inputString + " in Long is " +
+			 * ans);
+			 */
+
+			inputString = "5635ababch";
+			System.out.println("\nINPUT: " + inputString);
+			ans = stringToLong(inputString.toCharArray());
+			System.out.println("String " + inputString + " in Long is " + ans);
+
+		} catch (NumberFormatException numberFormatException) {
+			System.out.println(numberFormatException.toString());
+
+		} catch (Exception exception) {
+			System.out.println(exception.toString());
+
+		}
+
+	}
+}
---
+++ src/tree/TrinaryTree.java
@@ -0,0 +1,382 @@
+package tree;
+
+/**
+ * This is the Tri-nary tree class.
+ * 
+ * @author Rtndp
+ * 
+ */
+public class TrinaryTree {
+
+	/**
+	 * ROOT of the tree
+	 */
+	private Node root;
+
+	/**
+	 * This makes up the Node of the tree. Currently, this can only hold integer
+	 * values, as this is written to demonstrate the various operations on a
+	 * tri-nary tree, for e.g. INSERT, DELETE, PRINT.
+	 * 
+	 */
+	private class Node {
+		private Node left, right, center;
+		private int value;
+
+		/**
+		 * Parameterless constructor.
+		 */
+		@SuppressWarnings("unused")
+		public Node() {
+
+		}
+
+		/**
+		 * Parameterized Constructor. This is usually called while inserted a
+		 * new node into the tree.
+		 * 
+		 * @param value
+		 */
+		public Node(int value) {
+			this.left = null;
+			this.right = null;
+			this.center = null;
+			this.value = value;
+		}
+
+		/**
+		 * The toString() method is overwritten in order to be able to easily
+		 * display the contents of the node when required.
+		 */
+		@Override
+		public String toString() {
+			return "Node [left=" + left + ", right=" + right + ", center="
+					+ center + ", value=" + value + "]";
+		}
+
+	}
+
+	/**
+	 * This public method is called on a TrinaryTree object to insert a
+	 * node in the tree.
+	 * 
+	 * @param value
+	 * 
+	 */
+	public void insert(int value) {
+		root = insertHelper(root, value);
+	}
+
+	/**
+	 * This private helper function assists the public insert method in
+	 * inserting a node in its right position within the tree.
+	 * 
+	 * @param root
+	 * @param value
+	 * @return root
+	 */
+	private Node insertHelper(Node root, int value) {
+		/**
+		 * If the tree is initially empty, i.e., inserting the first node.
+		 */
+		if (root == null) {
+			return new Node(value);
+		}
+
+		/**
+		 * Similar to the BST, if the value of the node to be inserted is less
+		 * than the root, insertHelper is recursively called on the left subtree.
+		 */
+		if (value < root.value)
+			root.left = insertHelper(root.left, value);
+
+		/**
+		 * and if the value of the node to be inserted is greater than the root,
+		 * insertHelper is recursively called on the right subtree.
+		 */
+		else if (value > root.value)
+			root.right = insertHelper(root.right, value);
+
+		/**
+		 * This condition is written with the purpose of handling the special
+		 * case when the value of the node to be inserted is equal to the root.
+		 * 
+		 */
+		else if (value == root.value)
+			root.center = insertHelper(root.center, value);
+		else
+
+			/***
+			 * Once the appropriate position for the new node is found, the new
+			 * node is affixed to it appropriate pointer from the previous call.
+			 */
+			root.value = value;
+
+		return root;
+	}
+
+	/**
+	 * This public method is called on a TrinaryTree object to delete a
+	 * specified node in the tree.
+	 * 
+	 * @param key
+	 * 
+	 */
+	public void delete(int key) {
+		root = deleteHelper(root, key);
+	}
+
+	/**
+	 * This private helper function assists the public delete method in deleting
+	 * the node with the specified key.
+	 * 
+	 * Assumption - The tree structure can have multiple nodes with the same
+	 * value, therefore this delete method deletes only one node with the
+	 * specified key.
+	 * 
+	 * @param root
+	 * @param key
+	 * @return
+	 */
+	private Node deleteHelper(Node root, int key) {
+		/**
+		 * This condition handles the case where the tree is null
+		 */
+		if (root == null){
+			System.out.println("Node with value - "+key+" not found");
+			return root = null;
+		
+		}
+		/**
+		 * Similar to traditional BST, the search for the required key proceeds
+		 * recursively through the left subtree, if the key < root.value or the
+		 * right subtree, if the key > root.value
+		 */
+		if (key < root.value)
+			root.left = deleteHelper(root.left, key);
+
+		else if (key > root.value)
+			root.right = deleteHelper(root.right, key);
+		else {
+			/**
+			 * If the required key is found at some node in the tree. Two
+			 * conditions arise.
+			 * 
+			 * Condition 1 - it may have a center child node [which is equal to
+			 * the its own value], in which case, the node to be deleted is
+			 * replaced with its center child.
+			 */
+			if (root.center != null) {
+				root.center.left = root.left;
+				root.center.right = root.right;
+				return root.center;
+			} else {
+				/**
+				 * Condition 2 - it may not have a center child node, in which
+				 * case, the delete processed similar to the delete in BST,
+				 * taking three cases into consideration.
+				 * 
+				 * Case 1 - The node to be deleted is the leaf node
+				 */
+
+				if (root.right == null && root.left == null)
+					return root = null;
+
+				/**
+				 * Case 2 - The node to be deleted has only one child, the left
+				 * child (subtree), in which case, replace the node to be
+				 * deleted with its immediate left child
+				 */
+				if (root.right == null)
+					return root.left;
+
+				/**
+				 * Case 2 (contd.) - The node to be deleted has only one child,
+				 * the right child (subtree), in which case, replace the node to
+				 * be deleted with its immediate left child
+				 */
+				if (root.left == null)
+					return root.right;
+
+				/**
+				 * Case 3 - The node to be deleted has both its children, in
+				 * which case replace the node to be deleted with the minimum
+				 * node in its right subtree
+				 */
+
+				Node temp = root;
+				root = findMin(temp.right);
+				root.right = removeMin(temp.right);
+				root.left = temp.left;
+
+			}
+
+		}
+		return root;
+
+	}
+
+	/**
+	 * This private helper function assists the delete method in looking up
+	 * the minimum value in the tree rooted at node passed as a parameter 
+	 * to this method
+	 *  
+	 * @param root
+	 * @return
+	 */
+	private Node findMin(Node root) {
+		
+		if (root.left == null)
+			return root;
+
+		/**
+		 * Keeps calling the left subtree, till the leaf is reached
+		 */
+		return findMin(root.left);
+	}
+	
+	/**
+	 * This private helper function assists the delete method in deleting the
+	 * smallest node in the tree or the specific subtree
+	 * 
+	 * @param root
+	 * @return
+	 */
+	private Node removeMin(Node root) {
+		if (root.left == null)
+			return root.right;
+		
+		root.left = removeMin(root.left);
+
+		return root;
+	}
+
+	/**
+	 * 
+	 * This public method is called on a TrinaryTree object to display the
+	 * entire tree.
+	 * 
+	 */
+	public void print() {
+		printHelper(root);
+		System.out.println();
+	}
+
+	/**
+	 * This private helper method assists the print method in displaying
+	 * the entire tree.
+	 * 
+	 * The traversal considered for this print method - INORDER
+	 * 
+	 * @param root
+	 */
+	private void printHelper(Node root) {
+		if (root == null) {
+			return;
+		}
+
+		printHelper(root.left);
+		printHelper(root.center);
+		System.out.print(root.value + " ");
+		printHelper(root.right);
+
+	}
+	
+	public static void main(String[] args) {
+		TrinaryTree tree = new TrinaryTree();
+
+		System.out.println("---");
+		tree.delete(9);
+		tree.print();
+		
+		tree.insert(5);
+		
+		tree.insert(4);
+		
+		tree.insert(9);
+		
+		System.out.println("---");
+		tree.insert(9);
+		tree.print();
+		
+		tree.insert(5);
+		
+		tree.insert(7);
+		
+		System.out.println("---");
+		tree.insert(2);
+		tree.print();
+		
+		
+		tree.insert(2);
+		
+		tree.insert(5);
+		
+		tree.insert(5);
+		
+		tree.insert(3);
+		
+		tree.insert(12);
+		
+		tree.insert(11);
+		
+		tree.insert(13);
+		
+		tree.insert(6);
+		
+		tree.insert(1);
+
+		System.out.println("---");
+		tree.print();
+
+		System.out.println("---");
+		tree.delete(9);
+		tree.print();
+
+		System.out.println("---");
+		tree.delete(9);
+		tree.print();
+
+		System.out.println("---");
+		tree.delete(18);
+		tree.print();
+
+		System.out.println("---");
+		tree.delete(11);
+		tree.print();
+
+		System.out.println("---");
+		tree.delete(13);
+		tree.print();
+
+		System.out.println("---");
+		tree.delete(5);
+		tree.print();
+
+		System.out.println("---");
+		tree.delete(5);
+		tree.print();
+
+		System.out.println("---");
+		tree.delete(5);
+		tree.print();
+
+		System.out.println("---");
+		tree.delete(5);
+		tree.print();
+
+		System.out.println("---");
+		tree.delete(5);
+		tree.print();
+
+		System.out.println("---");
+		tree.delete(5);
+		tree.print();
+
+		System.out.println("---");
+		tree.delete(5);
+		tree.print();
+
+	}
+
+}
